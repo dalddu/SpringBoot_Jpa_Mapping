486aad8b72577b8e1ff1d002472da59527d385f3
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-11-23 14:24:52 +0900

    BookController

---
+++ src/main/java/com/example/Author_exercise/controller/BookController.java
@@ -0,0 +1,40 @@
+package com.example.Author_exercise.controller;
+
+
+import com.example.Author_exercise.domain.Book;
+import com.example.Author_exercise.domain.dto.BookResponse;
+import com.example.Author_exercise.repository.BookRepository;
+import com.example.Author_exercise.service.BookService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+
+    private final BookService bookService;
+
+
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<BookResponse>> list(Pageable pageable){
+        return ResponseEntity.ok().body(bookService.findBooks(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> get(@PathVariable Long id){
+        return ResponseEntity.ok().body(new Book());
+    }
+}
