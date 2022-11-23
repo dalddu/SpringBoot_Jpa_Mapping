6f37848850a46993b02bb6b00c9f7bae874ba6e0
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-11-23 14:24:39 +0900

    Book

---
+++ src/main/java/com/example/Author_exercise/domain/Book.java
@@ -0,0 +1,20 @@
+package com.example.Author_exercise.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    @Id
+    private Long id;
+    private String name;
+    private Long authorId;
+}
